6b310698f1b1af3d58d038d6fcb88fa3ca199ce0
osvegn <thomas.prudhomme@epitech.eu>
2022-04-26 22:22:25 +0200

    [+] add epitech header in parser.hpp and parser.cpp files

---
+++ src/Parser/Parser.cpp
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2022
+** plaza
+** File description:
+** Parser
+*/
+
+#include "Parser.hpp"
+
+Parser::Parser()
+{
+}
+
+Parser::~Parser()
+{
+}
---
+++ src/Parser/Parser.hpp
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2022
+** plaza
+** File description:
+** Parser
+*/
+
+#pragma once
+
+class Parser {
+    public:
+        Parser();
+        ~Parser();
+
+    protected:
+    private:
+};
