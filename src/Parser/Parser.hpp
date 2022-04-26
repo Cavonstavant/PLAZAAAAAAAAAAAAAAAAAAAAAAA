9bd65b32983b42e86810deddb43861112184f415
Restyled.io <commits@restyled.io>
2022-04-26 20:56:53 +0000

    Restyled by astyle

--- src/Parser/Parser.hpp
+++ src/Parser/Parser.hpp
@@ -10,11 +10,11 @@
 #include <string>
 
 class Parser {
-    public:
-        Parser();
-        ~Parser();
-        void setCommand(const std::string &);
-        const std::string &getCommand() const;
-    private:
-        std::string _command;
+public:
+    Parser();
+    ~Parser();
+    void setCommand(const std::string &);
+    const std::string &getCommand() const;
+private:
+    std::string _command;
 };
