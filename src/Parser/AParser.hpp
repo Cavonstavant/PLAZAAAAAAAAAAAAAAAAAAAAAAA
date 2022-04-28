5790dbc0b614b18d6fecc4734c5055d521998bf3
Restyled.io <commits@restyled.io>
2022-04-28 14:52:43 +0000

    Restyled by clang-format

--- src/Parser/AParser.hpp
+++ src/Parser/AParser.hpp
@@ -15,68 +15,68 @@
  *
  */
 class AParser : public IParser {
-public:
-    /**
+    public:
+        /**
      * @brief Destroy the AParser object as default.
      *
      */
-    ~AParser() = default;
-    /**
+        ~AParser() = default;
+        /**
      * @brief Set the command that will be parsed.
      *
      */
-    void setCommand(const std::string &);
-    /**
+        void setCommand(const std::string &);
+        /**
      * @brief Get the command previously set.
      *
      * @return const std::string& of the command.
      * It return an empty std::string in case of undefined command.
      */
-    const std::string &getCommand() const;
-    /**
+        const std::string &getCommand() const;
+        /**
      * @brief Get the number of arguments in the command.
      *
      * @return const std::size_t&, the number of arguments.
      */
-    const std::size_t &getArgumentNumber() const;
-    /**
+        const std::size_t &getArgumentNumber() const;
+        /**
      * @brief This function will be set differently by the children
      *
      */
-    virtual void prossessArguments() = 0;
-    /**
+        virtual void prossessArguments() = 0;
+        /**
      * @brief Set a char that will be the separator on each command.
      *
      */
-    void setCommandSeparator(char);
-    /**
+        void setCommandSeparator(char);
+        /**
      * @brief Get the a list of all arguments in the command.
      *
      * @return const std::vector<std::string>& that contain each argument.
      */
-    const std::vector<std::string> &getArguments() const;
+        const std::vector<std::string> &getArguments() const;
 
-protected:
-    /**
+    protected:
+        /**
      * @brief Store the command that will be prossess.
      *
      */
-    std::string _command;
-    /**
+        std::string _command;
+        /**
      * @brief Store the number of arguments in the command.
      *
      */
-    std::size_t _argumentNumber;
-    /**
+        std::size_t _argumentNumber;
+        /**
      * @brief Store the separator of each argument.
      *
      */
-    char _separator;
-    /**
+        char _separator;
+        /**
      * @brief Store a vector of all argument.
      *
      */
-    std::vector<std::string> _arguments;
+        std::vector<std::string> _arguments;
 };
 
 #endif
