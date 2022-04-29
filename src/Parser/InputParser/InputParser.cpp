03c9078df5e156982bb78db0f5fede95bb109342
osvegn <thomas.prudhomme@epitech.eu>
2022-04-29 09:32:44 +0200

    [+] create InputParser.hpp/.cpp #4

---
+++ src/Parser/InputParser/InputParser.cpp
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2022
+** plaza
+** File description:
+** InputParser
+*/
+
+#include "InputParser.hpp"
+
+InputParser::InputParser()
+{
+    _command = "";
+    _argumentNumber = 0;
+    _separator = ' ';
+    _arguments.clear();
+}
+
+InputParser::~InputParser()
+{
+}
---
+++ src/Parser/InputParser/InputParser.hpp
@@ -0,0 +1,24 @@
+/*
+** EPITECH PROJECT, 2022
+** plaza
+** File description:
+** InputParser
+*/
+
+#ifndef INPUTPARSER_HPP_
+#define INPUTPARSER_HPP_
+
+#include "AParser.hpp"
+
+/// \brief InputParser is a class use to parse command of pizza while the program is running
+class InputParser : public AParser {
+    public:
+        /// \brief Create the InputParser instance and define all variables
+        InputParser();
+        /// \brief Destroy the InputParser instance and destroy all variables
+        ~InputParser();
+        /// \brief Prossess the argument to set them in a form usefull for the program
+        void prossessArguments() override;
+};
+
+#endif
