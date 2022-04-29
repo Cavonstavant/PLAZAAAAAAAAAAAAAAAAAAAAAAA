4c80915970cb3963da34c365d2676e22748d950f
Nathan <nathan.rousseau@epitech.eu>
2022-04-29 14:18:39 +0200

    [+] - ArgumentsParser Class created, main loop done #3

--- src/Parser/ArgumentsParser/ArgumentsParser.cpp
+++ src/Parser/ArgumentsParser/ArgumentsParser.cpp
@@ -7,10 +7,67 @@
 
 #include "ArgumentsParser.hpp"
 
-ArgumentsParser::ArgumentsParser()
+ArgumentsParser::ArgumentsParser(const char **& args)
 {
+    for (size_t x = 0; args[x]; ++x) {
+        _arguments.push_back(std::string(args[x]));
+    }
+    _argumentNumber = _arguments.size();
 }
 
-ArgumentsParser::~ArgumentsParser()
+void ArgumentsParser::prossessArguments()
 {
+    if (_argumentNumber != 3)
+        return; //Throw an Exception
+    for (size_t x = _arguments.size() - 1; x > 0; --x) {
+        try {
+            size_t value = std::stoi(_arguments[x]);
+
+            switch (x) {
+            case 3:
+                setRefillTime(value);
+                break;
+            case 2:
+                setCookNumber(value);
+                break;
+            case 1:
+                setCookingTime(value);
+                break;
+            default:
+                break; // raise
+            }
+        } catch (...) {
+            return; //raise
+        }
+    }
+}
+
+size_t ArgumentsParser::getCookingTime(void) const
+{
+    return _cookingTime;
+}
+
+size_t ArgumentsParser::getRefillTime(void) const
+{
+    return _refillTimeMs;
+}
+
+size_t ArgumentsParser::getCookNumber(void) const
+{
+    return _cookNumber;
+}
+
+void ArgumentsParser::setCookingTime(size_t cookingTime)
+{
+    _cookingTime = cookingTime;
+}
+
+void ArgumentsParser::setRefillTime(size_t refillTime)
+{
+    _refillTimeMs = refillTime;
+}
+
+void ArgumentsParser::setCookNumber(size_t cookNumber)
+{
+    _cookNumber = cookNumber;
 }
--- src/Parser/ArgumentsParser/ArgumentsParser.hpp
+++ src/Parser/ArgumentsParser/ArgumentsParser.hpp
@@ -10,14 +10,32 @@
 
 #include "AParser.hpp"
 
+#include <vector>
+#include <string>
+
 class ArgumentsParser : public AParser{
     public:
-        ArgumentsParser();
-        ~ArgumentsParser();
+        ArgumentsParser(const char **&);
 
+        /// \brief Destroy the AParser object as default.
+        ~ArgumentsParser() = default;
+
+        size_t getCookingTime(void) const;
+        size_t getCookNumber(void) const;
+        size_t getRefillTime(void) const;
+
+        void prossessArguments();
     protected:
 
     private:
+
+        void setCookingTime(size_t);
+        void setCookNumber(size_t cookNumber);
+        void setRefillTime(size_t refillTime);
+
+        size_t _cookNumber;
+        size_t _cookingTime;
+        size_t _refillTimeMs;
 
 };
 
