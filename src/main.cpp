c1f8156bf61d206deea7a9fe4ec9d94d774080b8
Nathan <nathan.rousseau@epitech.eu>
2022-04-29 16:12:14 +0200

    [+] - Add Arguments Handling #3

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,12 @@
 */
 
 #include <string>
+#include "Parser/ArgumentsParser/ArgumentsParser.hpp"
 
 int main(int ac, char **av)
 {
+    ArgumentsParser args (av + 1);
+
+    args.prossessArguments();
     return 0;
 }
