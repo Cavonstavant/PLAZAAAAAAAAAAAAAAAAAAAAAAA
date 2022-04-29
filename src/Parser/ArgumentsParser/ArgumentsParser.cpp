4a3581774c607238d093fa9fe86431b818dfa107
Nathan <nathan.rousseau@epitech.eu>
2022-04-29 11:15:38 +0200

    [+] - Adding Arguments Parser Files

---
+++ src/Parser/ArgumentsParser/ArgumentsParser.cpp
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2022
+** PLAZAAAAAAAAAAAAA
+** File description:
+** ArgumentsParser
+*/
+
+#include "ArgumentsParser.hpp"
+
+ArgumentsParser::ArgumentsParser()
+{
+}
+
+ArgumentsParser::~ArgumentsParser()
+{
+}
---
+++ src/Parser/ArgumentsParser/ArgumentsParser.hpp
@@ -0,0 +1,24 @@
+/*
+** EPITECH PROJECT, 2022
+** PLAZAAAAAAAAAAAAA
+** File description:
+** ArgumentsParser
+*/
+
+#ifndef ARGUMENTSPARSER_HPP_
+#define ARGUMENTSPARSER_HPP_
+
+#include "AParser.hpp"
+
+class ArgumentsParser : public AParser{
+    public:
+        ArgumentsParser();
+        ~ArgumentsParser();
+
+    protected:
+
+    private:
+
+};
+
+#endif /* !ARGUMENTSPARSER_HPP_ */
