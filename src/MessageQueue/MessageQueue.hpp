e1fcaff4a52b38c965ff53f07c2091805cca910d
Cavonstavant <constant.vigneron@protonmail.com>
2022-04-30 19:13:41 +0200

    [*] Update MessageQueue class methods

--- src/MessageQueue/MessageQueue.hpp
+++ src/MessageQueue/MessageQueue.hpp
@@ -17,35 +17,91 @@ extern "C" {
 #include <string>
 
 /// \brief Wrapper class around the XSI message queues.
-/// \note This class aim to provide a custom interface to manipulate XSI message queues by simply send enqueue and dequeue messages with send_message and receive_message.
+/// \note This class aim to provide a custom interface to manipulate XSI message queues by simply enqueue and dequeue messages with send_message and receive_message.
 class MessageQueue {
     public:
+        /// \brief Default Constructor, no queue is created when called.
         MessageQueue() = default;
+
+        /// \brief Destructor, the queue is destroyed / emptied when called.
         ~MessageQueue() = default;
-        MessageQueue &operator=(const MessageQueue &) = default;
-        MessageQueue(const MessageQueue &) = default;
+
+        /// \brief Creating a new queue from a already existing queue shall not be possible.
+        MessageQueue &operator=(const MessageQueue &) = delete;
+
+        /// \brief Creating a new queue from a already existing queue shall not be possible.
+        MessageQueue(const MessageQueue &) = delete;
+
+        /// \brief Retrieve the current text representation of the last message received.
+        /// \warning Calling this method without setting up the queue, receiving a message or sending first will result in a <b>Undefined Behavior</b>.
         inline std::string getMessage() const { return _text; };
-        inline void setMessage(std::string text) { _text = text; };
+
+        /// \brief Get the current message queue key.
         inline key_t getKey() const { return _key; };
+
+        /// \brief Set the message queue key.
+        /// \note In order to create a new key, please use the MessageQueue::createKey static method.
         inline void setKey(key_t key) { _key = key; };
-        void create_key(std::string pathname);
+
+        /// \brief C++ wrapper around C ftok().
+        /// \note This methods sets internally the key to be used.
+        void createKey(std::string pathname, int proj_id);
+
+        /// \brief Get the current message queue pathfile.
+        /// \warning Using thing method prior to the creation of a key results in a <b>Undefined Behavior</b>.
         inline std::string getPath() const { return _pathname; };
-        inline void setPath(std::string pathname) { _pathname = pathname; };
+
+        /// \brief Get the current message queue id.
+        /// \warning Using thing method prior to the creation of a key results in a <b>Undefined Behavior</b>.
+        /// \return The id returned by msgget().
         inline int getId() const { return _id; };
+
+        /// \brief Get the current message queue id.
+        /// \warning Using thing method prior to the creation of a key results in a <b>Undefined Behavior</b>.
         inline void setId(int id) { _id = id; };
-        void send_message(std::string message_text, long message_type);
-        std::string receive_message(long message_type);
+
+        /// \brief Sends a message to the queue.
+        /// \note This method is a C++ wrapper around msgsnd().
+        /// \warning Using thing method prior to the creation of a key results in a <b>Undefined Behavior</b>.
+        /// \param message The message to be sent.
+        /// \param message_type the type of the message.
+        void sendMessage(std::string message_text, long message_type);
+
+        /// \brief Receives a message from the queue.
+        /// \note This method is a C++ wrapper around msgrcv().
+        /// \warning Using thing method prior to the creation of a key results in a <b>Undefined Behavior</b>.
+        /// \param message_type the type of the message.
+        std::string receiveMessage(long message_type);
+
+        /// \brief Clear the queue.
+        /// \warning Using thing method prior to the creation of a key results in a <b>Undefined Behavior</b>.
         void clear();
     private:
+
+        /// \brief Inner representation of a message used by msgsnd() and msgrcv().
         struct message {
+            /// \brief The messsage type.
             long type;
+            /// \brief The message text.
             char text[256];
         };
+
+        /// \brief message instance to be used with msgsnd() and msgrcv().
         struct message _msg;
-        long _type;
+
+        /// \brief The text representation of the last message recieved.
         std::string _text;
+
+        /// \brief The type associated with the last message recieved.
+        long _type;
+
+        /// \brief The queue id returned by msgget().
         int _id;
+
+        /// \brief XSI key used to represent the queue.
         key_t _key;
+
+        /// \brief The pathfile of the queue.
         std::string _pathname;
 };
 
