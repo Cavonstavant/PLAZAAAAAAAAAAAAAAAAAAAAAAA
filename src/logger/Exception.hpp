b9dacbe15e14db6976847f37ccc5be0f198f8aad
Restyled.io <commits@restyled.io>
2022-05-01 16:19:14 +0000

    Restyled by astyle

--- src/logger/Exception.hpp
+++ src/logger/Exception.hpp
@@ -41,110 +41,110 @@
 
 /// \brief Base exception class for all exceptions in the project
 class PlazzaException : public std::exception {
-    public:
-        /// \brief DO NOT USE THIS CONSTRUCTOR, PLEASE USE THE MACROS DEFINED ABOVE IN EXCEPTION.HPP (PlazzaEX, FactoryEX, ...)
-        /// \param what The description of the exception
-        /// \param func The function where the exception is thrown
-        /// \param file The file where the exception is thrown
-        /// \param line The line where the exception is thrown
-        explicit PlazzaException(std::string const &what, std::string const &func, int const &line, std::string const &file);
-        /// \brief returns the description of the exception
-        /// \return the description of the exception
-        [[nodiscard]] const char *what() const noexcept override;
-        /// \brief returns where the exception is thrown
-        /// \return the where the exception has been thrown
-        [[nodiscard]] const std::string &where() const noexcept;
-        /// \brief returns the function where the exception is thrown
-        /// \return the function where the exception is thrown
-        [[nodiscard]] const std::string &getFunc() const noexcept;
-        /// \brief returns the file where the exception is thrown
-        /// \return the file where the exception is thrown
-        [[nodiscard]] const std::string &getFile() const noexcept;
-        /// \brief returns the line where the exception is thrown
-        /// \return the line where the exception is thrown
-        [[nodiscard]] const int &getLine() const noexcept;
-        /// \brief returns the name of the exception
-        /// \return the name of the exception
-        [[nodiscard]] const std::string &getName() const noexcept;
+public:
+    /// \brief DO NOT USE THIS CONSTRUCTOR, PLEASE USE THE MACROS DEFINED ABOVE IN EXCEPTION.HPP (PlazzaEX, FactoryEX, ...)
+    /// \param what The description of the exception
+    /// \param func The function where the exception is thrown
+    /// \param file The file where the exception is thrown
+    /// \param line The line where the exception is thrown
+    explicit PlazzaException(std::string const &what, std::string const &func, int const &line, std::string const &file);
+    /// \brief returns the description of the exception
+    /// \return the description of the exception
+    [[nodiscard]] const char *what() const noexcept override;
+    /// \brief returns where the exception is thrown
+    /// \return the where the exception has been thrown
+    [[nodiscard]] const std::string &where() const noexcept;
+    /// \brief returns the function where the exception is thrown
+    /// \return the function where the exception is thrown
+    [[nodiscard]] const std::string &getFunc() const noexcept;
+    /// \brief returns the file where the exception is thrown
+    /// \return the file where the exception is thrown
+    [[nodiscard]] const std::string &getFile() const noexcept;
+    /// \brief returns the line where the exception is thrown
+    /// \return the line where the exception is thrown
+    [[nodiscard]] const int &getLine() const noexcept;
+    /// \brief returns the name of the exception
+    /// \return the name of the exception
+    [[nodiscard]] const std::string &getName() const noexcept;
 
-    protected:
-        std::string _name;
-        std::string _what;
-        std::string _where;
-        std::string _func;
-        std::string _file;
-        int _line;
+protected:
+    std::string _name;
+    std::string _what;
+    std::string _where;
+    std::string _func;
+    std::string _file;
+    int _line;
 
-    private:
+private:
 };
 
 /// \brief Exception class for the factory
 class FactoryException : public PlazzaException {
-    public:
-        FactoryException(std::string const &what, std::string const &func, int const &line, std::string const &file) : PlazzaException(what, func, line, file)
-        {
-            _name = "FactoryException";
-        };
+public:
+    FactoryException(std::string const &what, std::string const &func, int const &line, std::string const &file) : PlazzaException(what, func, line, file)
+    {
+        _name = "FactoryException";
+    };
 };
 /// \brief Exception class for the factory if a file is invalid
 class InvalidFileException : public FactoryException {
-    public:
-        InvalidFileException(std::string const &what, std::string const &func, int const &line, std::string const &file) : FactoryException(what, func, line, file)
-        {
-            _name = "InvalidFileException";
-        };
+public:
+    InvalidFileException(std::string const &what, std::string const &func, int const &line, std::string const &file) : FactoryException(what, func, line, file)
+    {
+        _name = "InvalidFileException";
+    };
 };
 /// \brief Exception class for the factory if a file is not found
 class FileNotFoundException : public InvalidFileException {
-    public:
-        FileNotFoundException(std::string const &what, std::string const &func, int const &line, std::string const &file) : InvalidFileException(what, func, line, file)
-        {
-            _name = "FileNotFoundException";
-        };
+public:
+    FileNotFoundException(std::string const &what, std::string const &func, int const &line, std::string const &file) : InvalidFileException(what, func, line, file)
+    {
+        _name = "FileNotFoundException";
+    };
 };
 /// \brief Exception class for the factory if a file is corrupted
 class FileCorruptedException : public InvalidFileException {
-    public:
-        FileCorruptedException(std::string const &what, std::string const &func, int const &line, std::string const &file) : InvalidFileException(what, func, line, file)
-        {
-            _name = "FileCorruptedException";
-        };
+public:
+    FileCorruptedException(std::string const &what, std::string const &func, int const &line, std::string const &file) : InvalidFileException(what, func, line, file)
+    {
+        _name = "FileCorruptedException";
+    };
 };
 /// \brief Exception class for the factory if a file is unreadable
 class FileUnreadableException : public InvalidFileException {
-    public:
-        FileUnreadableException(std::string const &what, std::string const &func, int const &line, std::string const &file) : InvalidFileException(what, func, line, file)
-        {
-            _name = "FileUnreadableException";
-        };
+public:
+    FileUnreadableException(std::string const &what, std::string const &func, int const &line, std::string const &file) : InvalidFileException(what, func, line, file)
+    {
+        _name = "FileUnreadableException";
+    };
 };
 /// \brief Exception class for the libraries
 class LibraryException : public PlazzaException {
-    public:
-        LibraryException(std::string const &what, std::string const &func, int const &line, std::string const &file) : PlazzaException(what, func, line, file)
-        {
-            _name = "LibraryException";
-        };
+public:
+    LibraryException(std::string const &what, std::string const &func, int const &line, std::string const &file) : PlazzaException(what, func, line, file)
+    {
+        _name = "LibraryException";
+    };
 };
 /// \brief Exception class to throw when something is not implemented
 class NotImplementedException : public PlazzaException {
-    public:
-        NotImplementedException(std::string const &what, std::string const &func, int const &line, std::string const &file) : PlazzaException(what, func, line, file)
-        {
-            _name = "NotImplementedException";
-        };
+public:
+    NotImplementedException(std::string const &what, std::string const &func, int const &line, std::string const &file) : PlazzaException(what, func, line, file)
+    {
+        _name = "NotImplementedException";
+    };
 };
 /// \brief Exception class when a user is very stupid and we can't do what anything for them
 class VeryStupidUserException : public PlazzaException {
-    public:
-        explicit VeryStupidUserException(std::string const &what = "", std::string const &func = "", int const &line = 0, std::string const &file = "") : PlazzaException("Please stop", "You probably need to restart your project from scratch", 0, "User IQ")
-        {
-            _name = "VeryStupidUserException";
-            (void) what;
-            (void) func;
-            (void) line;
-            (void) file;
-        };
+public:
+    explicit VeryStupidUserException(std::string const &what = "", std::string const &func = "", int const &line = 0, std::string const &file = "") : PlazzaException("Please stop", "You probably need to restart your project from scratch", 0, "User IQ")
+    {
+        _name = "VeryStupidUserException";
+        (void) what;
+        (void) func;
+        (void) line;
+        (void) file;
+    };
 };
 
 #endif /* !PLAzZA_EXCEPTION_HPP */
