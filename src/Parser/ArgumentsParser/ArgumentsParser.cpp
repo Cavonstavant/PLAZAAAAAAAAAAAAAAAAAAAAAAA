9e9ff04d2c147558a6466a27474f7acbf2d4a7a7
Restyled.io <commits@restyled.io>
2022-05-02 08:38:25 +0000

    Restyled by clang-format

--- src/Parser/ArgumentsParser/ArgumentsParser.cpp
+++ src/Parser/ArgumentsParser/ArgumentsParser.cpp
@@ -8,7 +8,7 @@
 #include "ArgumentsParser.hpp"
 #include <iostream>
 
-ArgumentsParser::ArgumentsParser(char ** args)
+ArgumentsParser::ArgumentsParser(char **args)
 {
     for (size_t x = 0; args[x]; ++x) {
         _arguments.push_back(std::string(args[x]));
@@ -20,29 +20,29 @@ void ArgumentsParser::prossessArguments()
 {
     if (_argumentNumber != 3) {
         std::cout << "Invalid Number of Args" << std::endl;
-        return; // raise
+        return;// raise
     }
     for (size_t x = _arguments.size() - 1; x > 0; --x) {
         try {
             size_t value = std::stoi(_arguments[x]);
 
             switch (x) {
-            case 2:
-                setRefillTime(value);
-                break;
-            case 1:
-                setCookNumber(value);
-                break;
-            case 0:
-                setCookingTime(value);
-                break;
-            default:
-                std::cout << "Problem" << std::endl;
-                break; // raise
+                case 2:
+                    setRefillTime(value);
+                    break;
+                case 1:
+                    setCookNumber(value);
+                    break;
+                case 0:
+                    setCookingTime(value);
+                    break;
+                default:
+                    std::cout << "Problem" << std::endl;
+                    break;// raise
             }
         } catch (...) {
             std::cout << "Arg isn't a int" << std::endl;
-            return; // raise
+            return;// raise
         }
     }
 }
--- src/Parser/ArgumentsParser/ArgumentsParser.hpp
+++ src/Parser/ArgumentsParser/ArgumentsParser.hpp
@@ -10,46 +10,46 @@
 
 #include "../AParser.hpp"
 
-#include <vector>
-#include <string>
 #include <exception>
+#include <string>
+#include <vector>
 
 /// \brief ArgumentsParser class define functions for parsing received arguments
 class ArgumentsParser : public AParser {
-public:
-    /// \brief Construct the ArgumentsParser with the received arguments without the binary name (argv + 1)
-    ArgumentsParser(char **);
-    /// \brief Destroy the ArgumentsParser object as default.
-    ~ArgumentsParser() = default;
-    /// \brief Get the cooking Time for the Plazza
-    /// \return const size_t &, the cooking time
-    const size_t &getCookingTime(void) const;
-    /// \brief Get the cooking Time for the Plazza
-    /// \return const size_t &, the number of cooks
-    const size_t &getCookNumber(void) const;
-    /// \brief Get the time to refill the fridge for the Plazza
-    /// \return const size_t &, the refill time
-    const size_t &getRefillTime(void) const;
-    /// \brief Process the arguments given in the char ** (transfers to the inherited member _arguments) by the constructor
-    void prossessArguments();
+    public:
+        /// \brief Construct the ArgumentsParser with the received arguments without the binary name (argv + 1)
+        ArgumentsParser(char **);
+        /// \brief Destroy the ArgumentsParser object as default.
+        ~ArgumentsParser() = default;
+        /// \brief Get the cooking Time for the Plazza
+        /// \return const size_t &, the cooking time
+        const size_t &getCookingTime(void) const;
+        /// \brief Get the cooking Time for the Plazza
+        /// \return const size_t &, the number of cooks
+        const size_t &getCookNumber(void) const;
+        /// \brief Get the time to refill the fridge for the Plazza
+        /// \return const size_t &, the refill time
+        const size_t &getRefillTime(void) const;
+        /// \brief Process the arguments given in the char ** (transfers to the inherited member _arguments) by the constructor
+        void prossessArguments();
 
-private:
-    /// \brief Set a new cooking Time with the given parameter
-    /// \param newCookingTime the new cooking time to be set
-    void setCookingTime(size_t newCookingTime);
-    /// \brief Set a new Cook Number with the given parameter
-    /// \param newCookNumber the new number of cook to be set
-    void setCookNumber(size_t newCookNumber);
-    /// \brief Set a new refill time with the given parameter
-    /// \param newRefillTime the new refill Time to be set
-    void setRefillTime(size_t newRefillTime);
+    private:
+        /// \brief Set a new cooking Time with the given parameter
+        /// \param newCookingTime the new cooking time to be set
+        void setCookingTime(size_t newCookingTime);
+        /// \brief Set a new Cook Number with the given parameter
+        /// \param newCookNumber the new number of cook to be set
+        void setCookNumber(size_t newCookNumber);
+        /// \brief Set a new refill time with the given parameter
+        /// \param newRefillTime the new refill Time to be set
+        void setRefillTime(size_t newRefillTime);
 
-    /// \brief Store the cook Number after processArguments() method is called
-    size_t _cookNumber;
-    /// \brief Store the cooking Time after processArguments() method is called
-    size_t _cookingTime;
-    /// \brief Store the refill Time after processArguments() method is called
-    size_t _refillTimeMs;
+        /// \brief Store the cook Number after processArguments() method is called
+        size_t _cookNumber;
+        /// \brief Store the cooking Time after processArguments() method is called
+        size_t _cookingTime;
+        /// \brief Store the refill Time after processArguments() method is called
+        size_t _refillTimeMs;
 };
 
 #endif /* !ARGUMENTSPARSER_HPP_ */
