df2f07da81e22b810aaa6804c3eb97409066414f
Cavonstavant <constant.vigneron@protonmail.com>
2022-05-04 10:44:46 +0200

    [+] Throw propre exception for each IPC  systemcall

--- src/MessageQueue/MessageQueue.cpp
+++ src/MessageQueue/MessageQueue.cpp
@@ -9,15 +9,15 @@
 #include "Exception.hpp"
 #include "Logger.hpp"
 #include <cstring>
+#include <cerrno>
 #include <string>
 
 void MessageQueue::generateKey(const std::string &pathname, int proj_id)
 {
     key_t key = ftok(pathname.c_str(), proj_id);
 
-    if (key == -1) {
-        exit(84);
-    }
+    if (key == -1)
+        throw MessageQueueEX(std::string("Could not generate IPC key with ftok: ") + std::strerror(errno), Logger::CRITICAL);
     this->_key = key;
 }
 void MessageQueue::sendMessage(const std::string &message_text, long message_type)
@@ -25,28 +25,29 @@ void MessageQueue::sendMessage(const std::string &message_text, long message_typ
     int message_id;
 
     if (message_text.length() >= MAX_MESSAGE_SIZE)
-        throw PlazzaEX("Message is too long", Logger::MEDIUM);
+        throw MessageTooLongEX("Message is too long", Logger::MEDIUM);
     message_id = msgget(this->_key, 0666 | IPC_CREAT);
     if (message_id == -1) {
         exit(84);
     }
     _msg = new MessageQueue::message{.type = message_type};
     std::strcpy(_msg->text, message_text.c_str());
-    if (msgsnd(message_id, (void *) &_msg, sizeof(_msg->text), 0) == -1) {
-        exit(84);
-    }
+    if (msgsnd(message_id, (void *) &_msg, sizeof(_msg->text), 0) == -1)
+        throw MessageQueueEX(std::string("Could not send message: ") + std::strerror(errno), Logger::CRITICAL);
     delete _msg;
 }
 
 std::string MessageQueue::receiveMessage(long message_type)
 {
     int message_id = msgget(this->_key, 0666 | IPC_CREAT);
-    MessageQueue::message *msg = nullptr;
+    ssize_t message_size;
+    MessageQueue::message *msg = new MessageQueue::message;
 
-    if (message_id == -1) {
-        exit(84);
-    }
-    msgrcv(message_id, (void *) &msg, sizeof(msg->text), message_type, 0);
+    if (message_id == -1)
+        throw MessageQueueEX(std::string("Could not receive message: ") + std::strerror(errno), Logger::CRITICAL);
+    message_size = msgrcv(message_id, (void *) &msg, sizeof(msg->text), message_type, 0);
+    if (message_size < 0)
+        throw MessageQueueEX(std::string("Could not receive message: ") + std::strerror(errno), Logger::CRITICAL);
     return msg->text;
 }
 
