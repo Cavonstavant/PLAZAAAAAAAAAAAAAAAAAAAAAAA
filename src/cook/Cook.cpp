ac7e382a8ded89ab2609f906f9ca1bd9fb876139
osvegn <thomas.prudhomme@epitech.eu>
2022-05-05 09:57:41 +0200

    [+] create cook.hpp/cpp

---
+++ src/cook/Cook.cpp
@@ -0,0 +1,9 @@
+/*
+** EPITECH PROJECT, 2022
+** plazza
+** File description:
+** Cook
+*/
+
+#include "Cook.hpp"
+
---
+++ src/cook/Cook.hpp
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2022
+** plazza
+** File description:
+** Cook
+*/
+
+#pragma once
+
+namespace plazza {
+    class Cook {
+        public:
+            Cook() = default;
+            ~Cook() = default;
+    };
+}
