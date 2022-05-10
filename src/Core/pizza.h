b0f588d4256017b648cba41c63ee6d861590afe3
osvegn <thomas.prudhomme@epitech.eu>
2022-05-10 15:06:22 +0200

    [+] add documentation for structure pizza

--- src/Core/pizza.h
+++ src/Core/pizza.h
@@ -5,6 +5,8 @@
 ** pizza
 */
 
+/// \file src/Core/pizza.h
+
 #ifndef PIZZA_H_
     #define PIZZA_H_
 
@@ -14,6 +16,7 @@ namespace pizza {
     typedef pizzaSize_e pizzaSize_t;
     typedef pizza_s pizza_t;
 
+    /// \brief Enumeration for available pizza size.
     enum pizzaSize_e {
         S = 1,
         M = 2,
@@ -22,9 +25,13 @@ namespace pizza {
         XXL = 16,
     };
 
+    /// \brief Structure for pizza.
     struct pizza_s {
+        /// \brief std::string Name of the pizza.
         std::string name;
+        /// \brief enum pizzaSize_e Size of the pizza.
         enum pizzaSize_e size;
+        /// \brief std::string Number of pizza.
         std::size_t number;
     };
 }
