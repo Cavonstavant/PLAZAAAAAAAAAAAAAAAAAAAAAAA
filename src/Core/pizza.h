390ea5c757b40b0c0957de98d6ce651685d8512d
Restyled.io <commits@restyled.io>
2022-05-10 13:10:33 +0000

    Restyled by astyle

--- src/Core/pizza.h
+++ src/Core/pizza.h
@@ -8,31 +8,31 @@
 /// \file src/Core/pizza.h
 
 #ifndef PIZZA_H_
-    #define PIZZA_H_
+#define PIZZA_H_
 
-    #include <string>
+#include <string>
 
 namespace pizza {
-    typedef pizzaSize_e pizzaSize_t;
-    typedef pizza_s pizza_t;
+typedef pizzaSize_e pizzaSize_t;
+typedef pizza_s pizza_t;
 
-    /// \brief Enumeration for available pizza size.
-    enum pizzaSize_e {
-        S = 1,
-        M = 2,
-        L = 4,
-        XL = 8,
-        XXL = 16,
-    };
+/// \brief Enumeration for available pizza size.
+enum pizzaSize_e {
+    S = 1,
+    M = 2,
+    L = 4,
+    XL = 8,
+    XXL = 16,
+};
 
-    /// \brief Structure for pizza.
-    struct pizza_s {
-        /// \brief std::string Name of the pizza.
-        std::string name;
-        /// \brief enum pizzaSize_e Size of the pizza.
-        enum pizzaSize_e size;
-        /// \brief std::string Number of pizza.
-        std::size_t number;
-    };
+/// \brief Structure for pizza.
+struct pizza_s {
+    /// \brief std::string Name of the pizza.
+    std::string name;
+    /// \brief enum pizzaSize_e Size of the pizza.
+    enum pizzaSize_e size;
+    /// \brief std::string Number of pizza.
+    std::size_t number;
+};
 }
 #endif /* !PIZZA_H_ */
