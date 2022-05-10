6f8b175a8e7177a418fb218d8109969f9c9a1c06
Restyled.io <commits@restyled.io>
2022-05-10 13:10:35 +0000

    Restyled by clang-format

--- src/Core/pizza.h
+++ src/Core/pizza.h
@@ -13,26 +13,26 @@
 #include <string>
 
 namespace pizza {
-typedef pizzaSize_e pizzaSize_t;
-typedef pizza_s pizza_t;
+    typedef pizzaSize_e pizzaSize_t;
+    typedef pizza_s pizza_t;
 
-/// \brief Enumeration for available pizza size.
-enum pizzaSize_e {
-    S = 1,
-    M = 2,
-    L = 4,
-    XL = 8,
-    XXL = 16,
-};
+    /// \brief Enumeration for available pizza size.
+    enum pizzaSize_e {
+        S = 1,
+        M = 2,
+        L = 4,
+        XL = 8,
+        XXL = 16,
+    };
 
-/// \brief Structure for pizza.
-struct pizza_s {
-    /// \brief std::string Name of the pizza.
-    std::string name;
-    /// \brief enum pizzaSize_e Size of the pizza.
-    enum pizzaSize_e size;
-    /// \brief std::string Number of pizza.
-    std::size_t number;
-};
-}
+    /// \brief Structure for pizza.
+    struct pizza_s {
+            /// \brief std::string Name of the pizza.
+            std::string name;
+            /// \brief enum pizzaSize_e Size of the pizza.
+            enum pizzaSize_e size;
+            /// \brief std::string Number of pizza.
+            std::size_t number;
+    };
+}// namespace pizza
 #endif /* !PIZZA_H_ */
