9144e30ee2263e38954572115e9b121a6c6bcd07
osvegn <thomas.prudhomme@epitech.eu>
2022-05-11 14:33:30 +0200

    [+] add pizzaType

--- src/Core/Pizza.hpp
+++ src/Core/Pizza.hpp
@@ -13,11 +13,17 @@
 #include <string>
 
 namespace plazza {
-    typedef pizzaSize_e pizzaSize_t;
-    typedef pizza_s pizza_t;
+
+    /// \brief Enumeration for available pizza type.
+    enum PizzaType {
+        Regina = 1,
+        Margarita = 2,
+        Americana = 4,
+        Fantasia = 8,
+    };
 
     /// \brief Enumeration for available pizza size.
-    enum pizzaSize_e {
+    enum PizzaSize {
         S = 1,
         M = 2,
         L = 4,
@@ -26,13 +32,14 @@ namespace plazza {
     };
 
     /// \brief Structure for pizza.
-    struct pizza_s {
-            /// \brief std::string Name of the pizza.
-            std::string name;
-            /// \brief enum pizzaSize_e Size of the pizza.
-            enum pizzaSize_e size;
-            /// \brief std::string Number of pizza.
+    struct Pizza {
+            /// \brief PizzaType Type of the pizza.
+            PizzaType type;
+            /// \brief PizzaSize Size of the pizza.
+            PizzaSize size;
+            /// \brief std::size_t Number of pizza.
             std::size_t number;
     };
-}// namespace pizza
+} // namespace pizza
+
 #endif /* !PIZZA_H_ */
