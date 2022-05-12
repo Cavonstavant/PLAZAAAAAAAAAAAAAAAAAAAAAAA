343bd631b7d8309b82b08f489822a9407b8692c9
osvegn <thomas.prudhomme@epitech.eu>
2022-05-12 20:40:24 +0200

    [+] overload operator- for ingredient structure

--- src/Core/Pizza.hpp
+++ src/Core/Pizza.hpp
@@ -11,6 +11,7 @@
 #define PIZZA_H_
 
 #include <string>
+#include "Exception.hpp"
 
 namespace plazza {
 
@@ -41,9 +42,38 @@ namespace plazza {
             std::size_t number;
     };
 
+    /// \brief Structure for ingredient.
     struct Ingredient {
+        /// \brief Name of the ingredient.
         std::string name;
+        /// \brief Number of ingredient.
         std::size_t number;
+        /// \brief Overload the operator- for the structure Ingredient.
+        /// \brief It will decrement the ingredient number by the ingredient number passed as parameter.
+        /// \param Ingredient & Reference to the ingredient.
+        /// \return Ingredient after remove Ingredient number passed as parameter.
+        Ingredient &operator-(Ingredient &i) {
+            if (this->name == i.name) {
+                if (this->number < i.number) {
+                    throw PlazzaEX("There is not enought " + i.name + ".", Logger::LOW);
+                } else {
+                    this->number -= i.number;
+                }
+            } else {
+                throw PlazzaEX("The two ingredients are differents (" + this->name + ", " + i.name + ").", Logger::LOW);
+            }
+        }
+        /// \brief Overload the operator- for the structure Ingredient.
+        /// \brief It will decrement the ingredient number by the number passed as parameter.
+        /// \param std::size_t & Reference to the number to remove.
+        /// \return Ingredient after remove number passed as parameter.
+        Ingredient &operator-(std::size_t &number) {
+            if (this->number < number) {
+                throw PlazzaEX("There is not enought " + this->name + ".", Logger::LOW);
+            } else {
+                this->number -= number;
+            }
+        }
     };
 }// namespace plazza
 
