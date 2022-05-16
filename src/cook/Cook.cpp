91dea7133fde028de61114d0572d2fab49d437fb
osvegn <thomas.prudhomme@epitech.eu>
2022-05-16 17:38:05 +0200

    [+] throw an error in cook part

--- src/cook/Cook.cpp
+++ src/cook/Cook.cpp
@@ -28,10 +28,8 @@ Cook::Cook(Pizza pizza, std::size_t multiplier)
 
 void Cook::cookPizza(Pizza pizza)
 {
-    if (_workInProgress) {
-        PlazzaEX("Cook is already working !", Logger::HIGH);
-        return;
-    }
+    if (_workInProgress)
+        throw PlazzaEX("Cook is already working !", Logger::HIGH);
     _pizza = pizza;
     _workInProgress = true;
     _setCookingTime();
