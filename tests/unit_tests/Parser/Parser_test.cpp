31455f1aa5fba8ee6c01dcbacde2a3c1d28b7b5e
osvegn <thomas.prudhomme@epitech.eu>
2022-05-17 10:29:06 +0200

    [+] add basic test for InputParser

--- tests/unit_tests/Parser/Parser_test.cpp
+++ tests/unit_tests/Parser/Parser_test.cpp
@@ -4,3 +4,45 @@
 ** File description:
 ** Parser_test
 */
+
+#include <criterion/criterion.h>
+#include "InputParser.hpp"
+
+Test(InputParser, test_InputParser1)
+{
+    plazza::InputParser p;
+
+    cr_assert_eq(1, 1);
+}
+
+Test(InputParser, test_InputParser2)
+{
+    plazza::InputParser p;
+    std::vector<std::string> v;
+
+    p.setCommand("regina L x1");
+    p.setCommandSeparator(' ');
+    p.processArguments();
+    v = p.getArguments();
+    cr_assert_eq(v.size(), 4);
+}
+
+Test(InputParser, test_InputParser3)
+{
+    plazza::InputParser p;
+
+    p.setCommand("regina XL x10");
+    p.processArguments();
+    cr_assert_str_eq(p.getCommand().c_str(), "regina XL x10");
+}
+
+Test(InputParser, test_InputParser4)
+{
+    plazza::InputParser p;
+    std::vector<plazza::Pizza> v;
+
+    p.setCommand("americana S x289");
+    p.processArguments();
+    v = p.getPizza();
+    cr_assert_eq(v.size(), 1);
+}
