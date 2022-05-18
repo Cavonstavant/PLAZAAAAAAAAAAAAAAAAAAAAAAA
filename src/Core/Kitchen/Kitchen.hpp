b7609f02b749c908902429c3963a64fb29985d60
Restyled.io <commits@restyled.io>
2022-05-18 07:33:45 +0000

    Restyled by clang-format

--- src/Core/Kitchen/Kitchen.hpp
+++ src/Core/Kitchen/Kitchen.hpp
@@ -20,79 +20,79 @@
 
 namespace plazza {
 
-/// \brief Enumeration for the different ingredients.
-enum Ingredients {
-    Tomato = 0,
-    Gruyere,
-    Ham,
-    Mushrooms,
-    Steak,
-    Eggplant,
-    GoatCheese,
-    ChiefLove,
-    IngredientNumber
-};
+    /// \brief Enumeration for the different ingredients.
+    enum Ingredients {
+        Tomato = 0,
+        Gruyere,
+        Ham,
+        Mushrooms,
+        Steak,
+        Eggplant,
+        GoatCheese,
+        ChiefLove,
+        IngredientNumber
+    };
 
-/// \brief The class used to manage all the cooks and the orders.
-class Kitchen {
-public:
-    /// \brief Creating a Kitchen without specifying the number of cooks should not be possible
-    Kitchen() = delete;
+    /// \brief The class used to manage all the cooks and the orders.
+    class Kitchen {
+        public:
+            /// \brief Creating a Kitchen without specifying the number of cooks should not be possible
+            Kitchen() = delete;
 
-    /// \brief Creating a Kitchen with a specified number of cooks
-    explicit Kitchen(unsigned long nbCooks, unsigned long refillTime) : _nbCooks(nbCooks), _refillTime(refillTime), _stopKitchen(false), _fridge(IngredientNumber) {};
+            /// \brief Creating a Kitchen with a specified number of cooks
+            explicit Kitchen(unsigned long nbCooks, unsigned long refillTime) : _nbCooks(nbCooks), _refillTime(refillTime), _stopKitchen(false), _fridge(IngredientNumber){};
 
-    /// \brief Destructor
-    ~Kitchen() = default;
+            /// \brief Destructor
+            ~Kitchen() = default;
 
-    /// \brief Starts the kitchen and init all the cooks
-    /// \warning The number of cooks must be specified before starting the kitchen
-    void start();
+            /// \brief Starts the kitchen and init all the cooks
+            /// \warning The number of cooks must be specified before starting the kitchen
+            void start();
 
-    /// \brief Stops the kitchen and joins all the cooks
-    void stop();
+            /// \brief Stops the kitchen and joins all the cooks
+            void stop();
 
-    /// \brief Adds an order to the kitchen
-    void enqueueJob(std::function<void()> &job);
+            /// \brief Adds an order to the kitchen
+            void enqueueJob(std::function<void()> &job);
 
-private:
-    /// \brief Main function instancied in each thread
-    static void _Cook(Kitchen *obj);
+        private:
+            /// \brief Main function instancied in each thread
+            static void _Cook(Kitchen *obj);
 
-    /// \brief fill the ingredients of the fridge 1 x timeToFill
-    /// \param timeToFill number of time to fill the fridge
-    static void _fillFridge(const std::size_t &timeToFill, Kitchen &obj);
+            /// \brief fill the ingredients of the fridge 1 x timeToFill
+            /// \param timeToFill number of time to fill the fridge
+            static void _fillFridge(const std::size_t &timeToFill, Kitchen &obj);
 
-    /// \brief check every timeToWait to fill the fridge
-    static void _waitToFillFridge(const std::size_t &timeToWait, Kitchen &obj);
+            /// \brief check every timeToWait to fill the fridge
+            static void _waitToFillFridge(const std::size_t &timeToWait, Kitchen &obj);
 
-    /// \brief The kitchen brigade regrouping all the cooks
-    std::vector<std::thread> _brigade;
+            /// \brief The kitchen brigade regrouping all the cooks
+            std::vector<std::thread> _brigade;
 
-    /// \brief The mutex used to protect the kitchen ingredients
-    std::mutex _mutex;
+            /// \brief The mutex used to protect the kitchen ingredients
+            std::mutex _mutex;
 
-    /// \brief The condition variable used to notify the cooks that there is an order
-    std::condition_variable order_condition;
+            /// \brief The condition variable used to notify the cooks that there is an order
+            std::condition_variable order_condition;
 
-    /// \brief The queue of orders comming from the reception
-    std::queue<std::function<void()>> _orders;
+            /// \brief The queue of orders comming from the reception
+            std::queue<std::function<void()>> _orders;
 
-    /// \brief The number of cooks in the kitchen
-    unsigned long _nbCooks;
+            /// \brief The number of cooks in the kitchen
+            unsigned long _nbCooks;
 
-    /// \brief The time to wait before fill the fridge
-    unsigned long _refillTime;
+            /// \brief The time to wait before fill the fridge
+            unsigned long _refillTime;
 
-    /// \brief Store the oldTime to fill
-    unsigned long _oldTime;
+            /// \brief Store the oldTime to fill
+            unsigned long _oldTime;
 
-    /// \brief The flag used to stop the kitchen
-    bool _stopKitchen;
+            /// \brief The flag used to stop the kitchen
+            bool _stopKitchen;
 
-    /// \brief The kitchen fridge, containing all of the ingredients
-    std::vector<Ingredient> _fridge;
-};
+            /// \brief The kitchen fridge, containing all of the ingredients
+            std::vector<Ingredient> _fridge;
+    };
 
 }// namespace plazza
 
