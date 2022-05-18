1614d39d28c3baae29758483315378e2c59b566f
Restyled.io <commits@restyled.io>
2022-05-18 19:57:20 +0000

    Restyled by clang-format

--- src/Core/Cook/Cook.hpp
+++ src/Core/Cook/Cook.hpp
@@ -19,42 +19,42 @@ extern "C" {
 }
 
 namespace plazza {
-/// \brief Cook class is use to cook a pizza with a duration time.
-class Cook {
-public:
-    /// \brief Default constructor
-    /// \param std::size_t Set a multiplier for the cooking time.
-    Cook(std::size_t multiplier = 1);
-    /// \brief Constructor with a pizza as parameter.
-    /// \param Pizza Set the pizza to be cook.
-    /// \param std::size_t Set a multiplier for the cooking time.
-    Cook(const Pizza, std::size_t multiplier = 1);
-    /// \brief Default destructor.
-    ~Cook() = default;
-    /// \brief Set the cooking time multiplier.
-    /// \param std::size_t Coef of the cooking time.
-    void setCookingTimeMultipiler(std::size_t multiplier);
-    /// \brief Set a pizza to be cook.
-    /// \param Pizza The pizza to be cook.
-    void cookPizza(Pizza pizza);
-    /// \brief Get the status of a cook.
-    /// \return true if the cook is cooking, false otherwise.
-    bool getCookStatus();
+    /// \brief Cook class is use to cook a pizza with a duration time.
+    class Cook {
+        public:
+            /// \brief Default constructor
+            /// \param std::size_t Set a multiplier for the cooking time.
+            Cook(std::size_t multiplier = 1);
+            /// \brief Constructor with a pizza as parameter.
+            /// \param Pizza Set the pizza to be cook.
+            /// \param std::size_t Set a multiplier for the cooking time.
+            Cook(const Pizza, std::size_t multiplier = 1);
+            /// \brief Default destructor.
+            ~Cook() = default;
+            /// \brief Set the cooking time multiplier.
+            /// \param std::size_t Coef of the cooking time.
+            void setCookingTimeMultipiler(std::size_t multiplier);
+            /// \brief Set a pizza to be cook.
+            /// \param Pizza The pizza to be cook.
+            void cookPizza(Pizza pizza);
+            /// \brief Get the status of a cook.
+            /// \return true if the cook is cooking, false otherwise.
+            bool getCookStatus();
 
-private:
-    /// \brief Set the cooking time depending of the pizza to be cook.
-    void _setCookingTime();
-    /// \brief Store the pizza to be cook.
-    Pizza _pizza;
-    /// \brief Store if the cook is working.
-    bool _workInProgress;
-    /// \brief Store the multiplier of cooking time.
-    std::size_t _multiplier;
-    /// \brief Store the start time of the cook.
-    time_t _start;
-    /// \brief Store the duration of the cooking time of the pizza.
-    std::size_t _duration;
-};
+        private:
+            /// \brief Set the cooking time depending of the pizza to be cook.
+            void _setCookingTime();
+            /// \brief Store the pizza to be cook.
+            Pizza _pizza;
+            /// \brief Store if the cook is working.
+            bool _workInProgress;
+            /// \brief Store the multiplier of cooking time.
+            std::size_t _multiplier;
+            /// \brief Store the start time of the cook.
+            time_t _start;
+            /// \brief Store the duration of the cooking time of the pizza.
+            std::size_t _duration;
+    };
 }// namespace plazza
 
 #endif
