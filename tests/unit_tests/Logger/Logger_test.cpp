e16a7e765bf5440a51629331ac2c32774cadb343
Cavonstavant <constant.vigneron@protonmail.com>
2022-05-20 15:24:17 +0200

    [+] Add pizzas specific logging #115

---
+++ tests/unit_tests/Logger/Logger_test.cpp
@@ -0,0 +1,31 @@
+/*
+** EPITECH PROJECT, 2022
+** PLAZAAAAAAAAAAAAAAAAAAAAAAA
+** File description:
+** Logger_test
+*/
+
+#include "PizzaLogger.hpp"
+#include <criterion/criterion.h>
+#include <criterion/redirect.h>
+#include <criterion/parameterized.h>
+
+using namespace plazza;
+
+Test(pizza_logger, log_pizza_order_received, .init=cr_redirect_stdout)
+{
+    PizzaLogger::logPizza("pizza", PizzaLogger::ACTION_ORDER_RECIEVED);
+    cr_assert_stdout_eq_str("pizza: ORDER RECIEVED\n");
+}
+
+Test(pizza_logger, log_pizza_cooking_started, .init=cr_redirect_stdout)
+{
+    PizzaLogger::logPizza("pizza", PizzaLogger::ACTION_COOKING_STARTED);
+    cr_assert_stdout_eq_str("pizza: COOKING STARTED\n");
+}
+
+Test(pizza_logger, log_pizza_cooking_finished, .init=cr_redirect_stdout)
+{
+    PizzaLogger::logPizza("pizza", PizzaLogger::ACTION_COOKING_FINISHED);
+    cr_assert_stdout_eq_str("pizza: COOKING FINISHED\n");
+}
