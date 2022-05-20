1817ee27b3b5e5767f9d4d566ed1d562c0be98a6
Restyled.io <commits@restyled.io>
2022-05-20 23:16:50 +0000

    Restyled by astyle

--- src/Core/Kitchen/Kitchen.hpp
+++ src/Core/Kitchen/Kitchen.hpp
@@ -20,128 +20,128 @@
 
 namespace plazza {
 
-    /// \brief Enumeration for the different ingredients.
-    enum Ingredients {
-        Tomato = 1,
-        Gruyere,
-        Ham,
-        Mushrooms,
-        Steak,
-        Eggplant,
-        GoatCheese,
-        Doe,
-        IngredientNumber
+/// \brief Enumeration for the different ingredients.
+enum Ingredients {
+    Tomato = 1,
+    Gruyere,
+    Ham,
+    Mushrooms,
+    Steak,
+    Eggplant,
+    GoatCheese,
+    Doe,
+    IngredientNumber
+};
+
+/// \brief The class used to manage all the cooks and the orders.
+class Kitchen {
+public:
+    /// \brief Creating a Kitchen without specifying the number of cooks should not be possible
+    Kitchen() = delete;
+
+    /// \brief Creating a Kitchen with a specified number of cooks
+    explicit Kitchen(unsigned long nbCooks, unsigned long refillTime, unsigned long cookingTime) : _nbCooks(nbCooks), _availCooks(nbCooks), _refillTime(refillTime), _cookingTime(cookingTime), _stopKitchen(false)
+    {
+        _fridge.resize(IngredientNumber);
     };
 
-    /// \brief The class used to manage all the cooks and the orders.
-    class Kitchen {
-        public:
-            /// \brief Creating a Kitchen without specifying the number of cooks should not be possible
-            Kitchen() = delete;
+    /// \brief Destructor
+    ~Kitchen() = default;
 
-            /// \brief Creating a Kitchen with a specified number of cooks
-            explicit Kitchen(unsigned long nbCooks, unsigned long refillTime, unsigned long cookingTime) : _nbCooks(nbCooks), _availCooks(nbCooks), _refillTime(refillTime), _cookingTime(cookingTime), _stopKitchen(false) 
-            {
-                _fridge.resize(IngredientNumber);
-            };
+    /// \brief Starts the kitchen and init all the cooks
+    /// \warning The number of cooks must be specified before starting the kitchen
+    void start();
 
-            /// \brief Destructor
-            ~Kitchen() = default;
+    /// \brief Stops the kitchen and joins all the cooks
+    void stop();
 
-            /// \brief Starts the kitchen and init all the cooks
-            /// \warning The number of cooks must be specified before starting the kitchen
-            void start();
+    /// \brief Adds an order to the kitchen
+    void enqueueJob(Pizza &pizza);
 
-            /// \brief Stops the kitchen and joins all the cooks
-            void stop();
+    /// \brief used to check if all the cooks have finished cooking
+    /// or still have order to process
+    bool isBusy();
 
-            /// \brief Adds an order to the kitchen
-            void enqueueJob(Pizza &pizza);
+    /// \brief MessageQueue recepting the command from the reception
+    std::shared_ptr<MessageQueue> commandQueue;
 
-            /// \brief used to check if all the cooks have finished cooking
-            /// or still have order to process
-            bool isBusy();
+    /// \brief get the Pizza Type from the Command
+    /// \param fullCommand the String received in the Queue
+    /// \return the pizza type
+    static PizzaType getTypeFromFullCommand(const std::string &fullCommand);
 
-            /// \brief MessageQueue recepting the command from the reception
-            std::shared_ptr<MessageQueue> commandQueue;
+    /// \brief get the number of the pizza from the Command
+    /// \param fullCommand the String received in the Queue
+    /// \return the quantity of the Pizza
+    static int getQuantityFromFullCommand(const std::string &FullCommand);
 
-            /// \brief get the Pizza Type from the Command
-            /// \param fullCommand the String received in the Queue
-            /// \return the pizza type
-            static PizzaType getTypeFromFullCommand(const std::string &fullCommand);
+    /// \brief get the necessary ingredients from the pizza type
+    /// \param toCook the Pizza to be filled with ingredients
+    /// \param type the type of the Pizza
+    static void getIngredientsFromPizzaType(Pizza &toCook, PizzaType type);
+    /// \brief Unpack std::string in Pizza.
+    /// \param std::string to be unpack.
+    /// \return Pizza unpacked.
+    static Pizza unpack(const std::string &order);
 
-            /// \brief get the number of the pizza from the Command
-            /// \param fullCommand the String received in the Queue
-            /// \return the quantity of the Pizza
-            static int getQuantityFromFullCommand(const std::string &FullCommand);
+private:
+    /// \brief Check if the command ask for available cook ("avail_cooks ?")
+    /// \param std::string & The command to be check
+    /// \return bool True if it is the correct command, false otherwise
+    static bool _isAvailableCook(std::string &);
 
-            /// \brief get the necessary ingredients from the pizza type
-            /// \param toCook the Pizza to be filled with ingredients
-            /// \param type the type of the Pizza
-            static void getIngredientsFromPizzaType(Pizza &toCook, PizzaType type);
-            /// \brief Unpack std::string in Pizza.
-            /// \param std::string to be unpack.
-            /// \return Pizza unpacked.
-            static Pizza unpack(const std::string &order);
+    /// \brief Main function for the Blocking thread on the message queue, getting the command and give it in the job queue
+    static void _receptCook(Kitchen *obj);
 
-        private:
-            /// \brief Check if the command ask for available cook ("avail_cooks ?")
-            /// \param std::string & The command to be check
-            /// \return bool True if it is the correct command, false otherwise
-            static bool _isAvailableCook(std::string &);
+    /// \brief Main function instancied in each thread
+    static void _Cook(Kitchen *obj);
 
-            /// \brief Main function for the Blocking thread on the message queue, getting the command and give it in the job queue
-            static void _receptCook(Kitchen *obj);
+    /// \brief Init the fridge at the Kitchen Creation
+    static void _initFridge(Kitchen *obj);
 
-            /// \brief Main function instancied in each thread
-            static void _Cook(Kitchen *obj);
+    /// \brief fill the ingredients of the fridge 1 x timeToFill
+    /// \param timeToFill number of time to fill the fridge
+    static void _fillFridge(const std::size_t &timeToFill, Kitchen *obj);
 
-            /// \brief Init the fridge at the Kitchen Creation
-            static void _initFridge(Kitchen *obj);
+    /// \brief check every timeToWait to fill the fridge
+    static void _waitToFillFridge(const std::size_t &timeToWait, Kitchen *obj);
 
-            /// \brief fill the ingredients of the fridge 1 x timeToFill
-            /// \param timeToFill number of time to fill the fridge
-            static void _fillFridge(const std::size_t &timeToFill, Kitchen *obj);
+    /// \brief The kitchen brigade regrouping all the cooks
+    std::vector<std::thread> _brigade;
 
-            /// \brief check every timeToWait to fill the fridge
-            static void _waitToFillFridge(const std::size_t &timeToWait, Kitchen *obj);
+    /// \brief The mutex used to protect the kitchen ingredients
+    std::mutex _mutex;
 
-            /// \brief The kitchen brigade regrouping all the cooks
-            std::vector<std::thread> _brigade;
+    /// \brief The mutex used to protect the refill of the kitchen ingredients
+    std::mutex _fridgeMutex;
 
-            /// \brief The mutex used to protect the kitchen ingredients
-            std::mutex _mutex;
+    /// \brief The condition variable used to notify the cooks that there is an order
+    std::condition_variable order_condition;
 
-            /// \brief The mutex used to protect the refill of the kitchen ingredients
-            std::mutex _fridgeMutex;
+    /// \brief The list of pizzas waiting to be cooked by the cooks
+    std::queue<Pizza> _orders;
 
-            /// \brief The condition variable used to notify the cooks that there is an order
-            std::condition_variable order_condition;
+    /// \brief The number of cooks in the kitchen
+    unsigned long _nbCooks;
 
-            /// \brief The list of pizzas waiting to be cooked by the cooks
-            std::queue<Pizza> _orders;
+    /// \brief The number of available cooks
+    unsigned long _availCooks;
 
-            /// \brief The number of cooks in the kitchen
-            unsigned long _nbCooks;
+    /// \brief The time to wait before fill the fridge
+    unsigned long _refillTime;
 
-            /// \brief The number of available cooks
-            unsigned long _availCooks;
+    /// \brief The multiplier for the cooking Time
+    float _cookingTime;
 
-            /// \brief The time to wait before fill the fridge
-            unsigned long _refillTime;
+    /// \brief Store the old time to refill the fridge
+    unsigned long _oldTime;
 
-            /// \brief The multiplier for the cooking Time
-            float _cookingTime;
+    /// \brief The flag used to stop the kitchen
+    bool _stopKitchen;
 
-            /// \brief Store the old time to refill the fridge
-            unsigned long _oldTime;
-
-            /// \brief The flag used to stop the kitchen
-            bool _stopKitchen;
-
-            /// \brief The kitchen fridge, containing all of the ingredients
-            std::vector<Ingredient> _fridge;
-    };
+    /// \brief The kitchen fridge, containing all of the ingredients
+    std::vector<Ingredient> _fridge;
+};
 
 }// namespace plazza
 
--- src/Core/Plazza.hpp
+++ src/Core/Plazza.hpp
@@ -18,77 +18,49 @@
 #include <string>
 
 namespace plazza {
-    /// \brief Reception Class containing the main loop of the program
-    class Reception {
-        public:
-            /// \brief Default Constructor of the Reception class DO NOT USE or it will lead to undefined comportements
-            Reception() = default;
-            /// \brief Construct the Reception with the necessary parameters for the method run()
-            /// \param cookingTime needs to be get of an ArgumentsParser Object with getCookingTime() method
-            /// \param cookNumber needs to be get of an ArgumentsParser Object with getCookNumber() method
-            /// \param refillTime needs to be get of an ArgumentsParser Object with getRefillTime() method
-            Reception(float cookingTime, size_t cookNumber, size_t refillTime);
-            /// \brief Default Constructor of the Reception
-            ~Reception() = default;
-            /// \brief Pack a Pizza in std::string.
-            /// \param Pizza to be pack.
-            /// \return std::string packed.
-            std::string pack(const Pizza &);
-            /// \brief Unpack std::string in Pizza.
-            /// \param std::string to be unpack.
-            /// \return Pizza unpacked.
-            Pizza unpack(const std::string &);
-            /// \brief Main function of the program
-            void run();
+/// \brief Reception Class containing the main loop of the program
+class Reception {
+public:
+    /// \brief Default Constructor of the Reception class DO NOT USE or it will lead to undefined comportements
+    Reception() = default;
+    /// \brief Construct the Reception with the necessary parameters for the method run()
+    /// \param cookingTime needs to be get of an ArgumentsParser Object with getCookingTime() method
+    /// \param cookNumber needs to be get of an ArgumentsParser Object with getCookNumber() method
+    /// \param refillTime needs to be get of an ArgumentsParser Object with getRefillTime() method
+    Reception(float cookingTime, size_t cookNumber, size_t refillTime);
+    /// \brief Default Constructor of the Reception
+    ~Reception() = default;
+    /// \brief Pack a Pizza in std::string.
+    /// \param Pizza to be pack.
+    /// \return std::string packed.
+    std::string pack(const Pizza &);
+    /// \brief Unpack std::string in Pizza.
+    /// \param std::string to be unpack.
+    /// \return Pizza unpacked.
+    Pizza unpack(const std::string &);
+    /// \brief Main function of the program
+    void run();
 
-        private:
-            /// \brief Close the Kitchens Queues and send a kill signal
-            void _cleanKitchens(void);
-
-            /// \brief Create a new Kitchen and it's associated Message Queue
-            void _createNewKitchen(void);
-
-            /// \brief Sends the `avail_slots?` command² to all the kitchens and updates `_busyCooks`
-            void _updateBusyCooks(void);
-
-            /// \brief return true or false wether there is a need to create a new kitchen
-            bool _isNewKitchenNeeded(int pizzaAmt);
-
-            int _isAvailableSlots(std::string);
-            /// \brief Display all of the kitchens current status
-            void _displayKitchensStatus(void);
-
-            /// \brief Handle the receive input
-            /// \return true if the input is valid, false otherwise
-            bool _handleInput(const std::string &input);
-
-            /// \brief Create a kitchen if necessary, and send the command to the child process
-            void _manageOrders(const InputParser &command);
-
-            /// \brief Sends amt of pizza to the kitchen referenced by it's id
-            /// \param pizzaToCook The collection of pizza to cook
-            /// \param amt The amount of pizza to send to the kitchen
-            /// \param kitchenPid The kitchen subprocess pid
-            void _sendPizza(std::vector<Pizza> &pizzaToCook, int amt, pid_t kitchenPid);
-
-            /// \brief Map of the different child process, with the linked queues
-            std::map<pid_t, std::shared_ptr<MessageQueue>> _kitchenMap;
-
-            /// \brief Store the cookingTime
-            float _cookingTime;
-
-            /// \brief Store the cookNumber
-            size_t _cookNumber;
-
-            /// \brief Store the refillTime
-            size_t _refillTime;
-
-            /// \brief Maps the number of available pizza slots to the pid of a forked kitchen
-            std::map<pid_t, unsigned int> _availSlots;
-
-            /// \brief Total number of available pizza slots
-            int _availSlotsTotal;
-    };
+private:
+    int _isAvailableSlots(std::string);
+    /// \brief Close the Kitchens Queues and send a kill signal
+    void _cleanKitchens(void);
+    /// \brief Display all of the kitchens current status
+    void _displayKitchensStatus(void);
+    /// \brief Handle the receive input
+    /// \return true if the input is valid, false otherwise
+    bool _handleInput(const std::string &input);
+    /// \brief Create a kitchen if necessary, and send the command to the child process
+    void _sendCommand(const InputParser &command);
+    /// \brief Map of the different child process, with the linked queues
+    std::map<pid_t, std::shared_ptr<MessageQueue>> _kitchenMap;
+    /// \brief Store the cookingTime
+    float _cookingTime;
+    /// \brief Store the cookNumber
+    size_t _cookNumber;
+    /// \brief Store the refillTime
+    size_t _refillTime;
+};
 }// namespace plazza
 
 #endif /* !PLAZZA_HPP_ */
