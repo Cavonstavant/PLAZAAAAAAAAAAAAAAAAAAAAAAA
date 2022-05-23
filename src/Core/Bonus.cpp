ad55e5be5d260987e5ca18abc7f0fc138af48b4c
osvegn <thomas.prudhomme@epitech.eu>
2022-05-23 19:40:42 +0200

    [+] start bonus

---
+++ src/Core/Bonus.cpp
@@ -0,0 +1,18 @@
+/*
+** EPITECH PROJECT, 2022
+** plazza
+** File description:
+** Bonus
+*/
+
+#include "Bonus.hpp"
+
+using namespace plazza;
+
+Bonus::Bonus()
+{
+}
+
+Bonus::~Bonus()
+{
+}
---
+++ src/Core/Bonus.hpp
@@ -0,0 +1,23 @@
+/*
+** EPITECH PROJECT, 2022
+** plazza
+** File description:
+** Bonus
+*/
+
+#ifndef BONUS_HPP_
+#define BONUS_HPP_
+
+#include <SFML/Graphics.hpp>
+#include "Plazza.hpp"
+
+namespace plazza {
+    class Bonus {
+        public:
+            Bonus();
+            ~Bonus();
+        private:
+    };
+}
+
+#endif /* !BONUS_HPP_ */
